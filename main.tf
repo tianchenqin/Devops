920eb265bfe1b96d17f200fbfd05232cdce0c267
Qin <uidq6087@contiwan.com>
2020-04-16 14:05:22 +0800

    newmaintf

--- main.tf
+++ main.tf
@@ -7,7 +7,23 @@ provider "aws" {
 resource "aws_instance" "TQFirstInstance" {
   ami           = "ami-0e855a53ec7c8057e"
   instance_type = "t2.micro"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
   tags = {
     Name = "terraform-example"
   }	
+}
+
+resource "aws_security_group" "webserverSG" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
